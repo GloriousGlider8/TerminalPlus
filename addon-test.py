820e55d4aafa22fe07c9b8bc6713e33d2deb7139
Sam <sam.g.plimmer@gmail.com>
2024-02-08 19:33:17 +0000

    Generated by T+ DEV: Test

--- addon-test.py
+++ addon-test.py
@@ -13,5 +13,5 @@ argsTxt = None"""
 print(c.Fore.GREEN + "Terminal + addons are working." + c.Style.RESET_ALL + "\nArgument Format:")
 for i in range(len(args)):
     print("[" + str(i) + "]: " + args[i])
-print(c.Fore.BLUE + "Get argument list with:" + c.Fore.LIGHTCYAN_EX + temp)
+print(c.Fore.BLUE + "Get argument list with:\n" + c.Fore.LIGHTCYAN_EX + temp)
 print(c.Fore.BLUE + "in your addon script." + c.Style.RESET_ALL)
