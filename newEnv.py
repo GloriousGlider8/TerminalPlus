7c3263e9981aa6f3e0c7a487a7292ae1eb816149
GloriousGlider8 <110254845+GloriousGlider8@users.noreply.github.com>
2024-02-08 18:41:05 +0000

    Update newEnv.py

--- newEnv.py
+++ newEnv.py
@@ -1,8 +1,9 @@
 import os
 import colorama as c
+import gg8lib
 
 clone = input("[directory] Environment Directory: ")
-name = input("[string] (DIR Name) Environment Name: ")
+name = input("[string] (DIR Name) Environment Name: ").upper()
 
 while not os.path.isdir(clone):
     print(c.Fore.RED + "Not a valid directory.\n" + c.Style.RESET_ALL)
@@ -22,14 +23,18 @@ temp = open(clone + "\\data\\li", "x")
 temp.close()
 temp = open(clone + "\\data\\termP.log", "x")
 temp.close()
-os.system("git clone https://github.com/GloriousGlider8/TerminalPlus \"" + clone + "\\code\"")
-os.rename(f"{clone}\\code\\addon-test.py", f"{clone}\\addons\\addon-test.py")
-os.system("rmdir \"" + clone + "\\code\\.git\" /S /Q")
-os.remove(clone + "\\code\\.gitignore")
 os.mkdir(os.getenv("AppData") + "\\TerminalPlus\\" + name)
 temp = open(os.getenv("AppData") + "\\TerminalPlus\\" + name + "\\PATH", "x")
 temp.write(clone)
 temp.close()
+os.system("git clone https://github.com/GloriousGlider8/TerminalPlus \"" + clone + "\\code\"")
+if gg8lib.selPrompt(["Standard", "Development"], [">", "!"], "What type?\nDEV will not delete the git repository or remove the origin upon removal!") == 0:
+    os.system("rmdir \"" + clone + "\\code\\.git\" /S /Q")
+    os.remove(clone + "\\code\\.gitignore")
+else:
+    temp = open(os.getenv("AppData") + "\\TerminalPlus\\" + name + "\\DEV", "x")
+    temp.close()
+os.rename(f"{clone}\\code\\addon-test.py", f"{clone}\\addons\\addon-test.py")
 temp = open(clone + "\\code\\data.py")
 temp1 = temp.readlines()
 temp.close()
