5039b6a4d16c5f61aa586e7c6da0821b6491882c
Sam <sam.g.plimmer@gmail.com>
2024-02-10 12:10:36 +0000

    Test

--- __main__.py
+++ __main__.py
@@ -640,9 +640,10 @@ while True:
                                     if i == " " or i == ", ":
                                         idx += 1
                                     else:
+                                        print(f"{str(len(matches))} {str(idx)}")
                                         while len(matches) < idx:
                                             matches.append("")
-                                            print("e")
+                                            print("e") 
                                         matches[idx] = matches[idx] + i
                                 for i in matches:
                                     print(i)
